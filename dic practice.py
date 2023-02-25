f9884caa8862ff4b6dc324dc0d10277b71320bc0
akanksha-ghadigavkar <124158388+akanksha-ghadigavkar@users.noreply.github.com>
2023-02-25 12:39:42 +0530

    Add files via upload

---
+++ dic practice.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 16:59:52 2021
+
+@author: matam
+
+europe = {'spain':'madrid','france':'paris',
+                'germany':'bonn', 
+               'norway':'oslo', 
+               'italy':'rome', 
+               'poland':'warsaw',
+               'australia':'vienna' }
+
+del europe['australia']
+
+print(europe)
+"""
+"""
+europe = {'spain':'madrid', 'france':'paris', 
+          'germany':'berlin', 'norway':'oslo' }
+
+europe["italy"]="rome"
+
+print(europe)
+
+europe["russia"]="warsaw"
+print(europe)
+"""
+"""
+europe={'spain': 'madrid', 'france': 'paris', 'germany': 'berlin',
+        'norway': 'oslo', 'italy': 'rome', 'russia': 'warsaw'}
+
+europe['russia']='x'
+europe['russia']='y'
+europe['russia']='a'
+europe['russia']='z'
+europe['russia']='moscow'
+"""
+"""
+a={"name":"mampi","skill":"python",
+   "city":"jarkhand"}
+print(type(a))
+print(a)
+print(a.keys())
+print(a.values())
+a.popitem()
+print(a)
+print(a["name"])
+print(a["city"])
+
+"""
+
+
+europe={'spain': 'madrid', 'france': 'paris', 'germany': 'berlin',
+        'norway': 'oslo', 'italy': 'rome', 'russia': 'warsaw'}
+
+europe['russia']='x'
+europe['russia']='y'
+europe['russia']='a'
+europe['russia']='z'
+europe['russia']='moscow'
+
+
+print(europe)
+
+
+
+print(europe)
